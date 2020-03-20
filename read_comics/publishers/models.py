c30ef1d79f57e4f75f054766605b27e55343c8fe
artem.vasin <artem.vasin>
2020-03-20 03:02:34 +0300

    new: dev: Publisher model: added slug field __str__ method and default sorting

--- read_comics/publishers/models.py
+++ read_comics/publishers/models.py
@@ -1,12 +1,22 @@
 from django.db import models
-from utils.models import ComicvineSyncModel
+from django_extensions.db.fields import AutoSlugField
+
+from utils.models import ComicvineSyncModel, slugify_function
 
 
 class Publisher(ComicvineSyncModel):
     name = models.TextField()
-    aliases = models.TextField()
-    short_description = models.TextField()
-    html_description = models.TextField()
+    aliases = models.TextField(null=True)
+    short_description = models.TextField(null=True)
+    html_description = models.TextField(null=True)
 
-    thumb_url = models.URLField(max_length=1000)
-    image_url = models.URLField(max_length=1000)
+    thumb_url = models.URLField(max_length=1000, null=True)
+    image_url = models.URLField(max_length=1000, null=True)
+
+    slug = AutoSlugField(populate_from=["name"], slugify_function=slugify_function)
+
+    class Meta:
+        ordering = ("name", )
+
+    def __str__(self):
+        return "[Publisher] %s (%d)" % (self.name, self.pk)
